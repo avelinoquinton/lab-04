e92fe0a8500d2d6ba97ea568a0e6715a5cfefe61
avelinoquinton <47160670+avelinoquinton@users.noreply.github.com>
2019-02-12 17:49:35 -0600

    Final changes
    The previous files did not feature useful names and numbers for the program to execute. I went back and changed it for the Equipment Test and am now re-uploading the file.

--- EquipmentTest.java
+++ EquipmentTest.java
@@ -7,18 +7,18 @@ public class EquipmentTest
     public void equipmentConstructorTest() throws AssertException
     {
         // Use the default constructor
-    	String equipTest = "noname/0,10.0,10.0,description";
+    	String equipTest = "Grenade/50,10.0,10.0,description";
         Equipment equipment = new Equipment(equipTest);
 
-        // The name should be "noname", color "unknown", height and weight 0.
+        // The name should be "Grenade", color "Olive", height and weight 10.0.
     	
-    	Assert.assertTrue(equipment.getName().equals("noname"));
+    	Assert.assertTrue(equipment.getName().equals("Grenade"));
     	Assert.assertTrue(equipment.getDescription().equals("description"));
-    	Assert.assertEquals(0, equipment.getCount());
+    	Assert.assertEquals(50, equipment.getCount());
     	Assert.assertEquals(10.0, equipment.getTotalWeight());
     	Assert.assertEquals(10.0, equipment.getTotalPrice());
     	
-    	String expectedToString = "Name: noname, Number: 0, Weight: 10.00 lbs, Price: $10.00 - description";
+    	String expectedToString = "Name: Grenade, Number: 50, Weight: 10.00 lbs, Price: $10.00 - description";
     	Assert.assertEquals(expectedToString, equipment.toString());
     }
 
